fc61e21af5979ad145e70e7867c7c75a97209328
serge_kamya <sergekamya@gmail.com>
2021-09-15 07:43:18 +0200

    added styling with semantic-ui in EmployeeList.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import EmployeeList from "./components/EmployeeList";
+import { Container, Header } from "semantic-ui-react";
 
 export class App extends Component {
   render() {
     return (
-      <div>
-        <h1 id="header">Employee management</h1>
+      <Container>
+        <Header size="huge" id="header"> Employee management</Header>
         <EmployeeList />{" "}
-      </div>
+      </Container>
     );
   }
 }
--- src/components/EmployeeList.jsx
+++ src/components/EmployeeList.jsx
@@ -1,33 +1,42 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-
+import React, { Component } from "react";
+import axios from "axios";
+import { Item } from "semantic-ui-react";
 
 export class EmployeeList extends Component {
-    state= {
-        employees: []
-      }
-    
-      componentDidMount() {
-        this.getEmployees()
-      }
-    
-      getEmployees = async () => {
-        let employeeData = await axios.get("https://reqres.in/api/users")
-        this.setState({ employees: employeeData.data.data })
-      }
-    render() {
-        let employeeList = this.state.employees.map(employee => {
-            return (
-              <li key={employee.id} className="employee-item">
-                <p> {employee.first_name} {employee.last_name}</p>
-              </li>
-            )
-          })
+  state = {
+    employees: [],
+  };
 
-        return (
-           <ul id="employee-list">{employeeList}</ul>
-        )
-    }
+  componentDidMount() {
+    this.getEmployees();
+  }
+
+  getEmployees = async () => {
+    let employeeData = await axios.get("https://reqres.in/api/users");
+    this.setState({ employees: employeeData.data.data });
+  };
+  render() {
+    let employeeList = this.state.employees.map((employee) => {
+      return (
+        <Item key={employee.id} className="employee-item">
+          <Item.Image
+            className="avatar"
+            circular
+            size="tiny"
+            alt={employee.first_name}
+            src={employee.avatar}
+          />
+          <Item.Content verticalAlign="middle">
+            <Item.Header className="name">
+              {employee.first_name} {employee.last_name}
+            </Item.Header>
+          </Item.Content>
+        </Item>
+      );
+    });
+
+    return <Item.Group id="employee-list">{employeeList}</Item.Group>;
+  }
 }
 
-export default EmployeeList
+export default EmployeeList;
