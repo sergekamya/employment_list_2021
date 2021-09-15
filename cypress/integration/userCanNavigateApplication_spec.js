ada4ddf0423af8c89b7d57abf64b45ef587f2e22
serge_kamya <sergekamya@gmail.com>
2021-09-15 06:58:01 +0200

    wrote user story in README.md and created spec in userCanNavigateApplication_spec.js

---
+++ cypress/integration/userCanNavigateApplication_spec.js
@@ -0,0 +1,23 @@
+describe('user visits the application', () => {
+    beforeEach(() => {
+        cy.visit("http://localhost:3000");
+    });
+
+    it("displays a header", () => {
+        cy.get("#header").should("contain.text", "Employee management");
+      });
+
+      it("is expected to list 6 times", () => {
+        cy.get("#employee-list").children().should("have.length", 6);
+      });
+
+      it("is expected to return Thomas as first employee in list ", () => {
+        cy.get("#employee-list")
+          .children()
+          .first()
+          .should("contain.text", "Thomas Bluth");
+      });
+
+
+    
+});
--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ export class App extends Component {
   render() {
     return (
       <div>
-        
+      <h1 id="header">Employee management</h1>
+      <div id="employee-list"></div>
       </div>
     )
   }
